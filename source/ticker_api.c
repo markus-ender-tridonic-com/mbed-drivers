b788dfc9859997fb51b012cfa56081a30a376bf8
markus.ender <Markus.Ender@tridonic.com>
2016-02-12 09:49:50 +0100

    correct handling of timeouts at a timer overflow

--- source/ticker_api.c
+++ source/ticker_api.c
@@ -14,8 +14,11 @@
  * limitations under the License.
  */
 #include <stddef.h>
+#include <stdbool.h>
 #include "ticker_api.h"
 #include "cmsis.h"
+
+inline bool ticker_timeIsInPeriod(timestamp_t start, timestamp_t time, timestamp_t end);
 
 void ticker_set_handler(const ticker_data_t *const data, ticker_event_handler handler) {
     data->interface->init();
@@ -34,17 +37,24 @@ void ticker_irq_handler(const ticker_data_t *const data) {
             return;
         }
 
-        if ((int)(data->queue->head->timestamp - data->interface->read()) <= 0) {
-            // This event was in the past:
+        timestamp_t ticksNow = data->interface->read();		// read actual timer value
+        uint32_t diff = data->queue->head->timestamp - ticksNow;
+        diff &= TICKER_TIME_MASK;
+        if ((diff <= TICKER_FUTURE_TOLERANCE) ||	// also execute event in 'near' future (are otherwise lost because of us-resolution)
+            (diff > TICKER_PAST_TOLERANCE)) 		// timestamp just missed (not 'too long' in the past)
+        {
+            // This events timeout is reached:
             //      point to the following one and execute its handler
             ticker_event_t *p = data->queue->head;
             data->queue->head = data->queue->head->next;
+
             if (data->queue->event_handler != NULL) {
                 (*data->queue->event_handler)(p->id); // NOTE: the handler can set new events
             }
             /* Note: We continue back to examining the head because calling the
              * event handler may have altered the chain of pending events. */
-        } else {
+        }
+        else {
             // This event and the following ones in the list are in the future:
             //      set it as next interrupt and return
             data->interface->set_interrupt(data->queue->head->timestamp);
@@ -57,6 +67,14 @@ void ticker_insert_event(const ticker_data_t *const data, ticker_event_t *obj, t
     /* disable interrupts for the duration of the function */
     __disable_irq();
 
+    timestamp_t actCnt = data->interface->read();		// read actual timer counter
+
+    /* reduce actual timer count because it counts up (us-resolution)
+     * while periodic events are executed and re-inserted;
+     * this can be a problem if timeout-counts are close together  */
+    actCnt -= TICKER_EXPECT_ISR_DELAY;
+    timestamp &= TICKER_TIME_MASK;						// limit timeout value acc. used timer
+
     // initialise our data
     obj->timestamp = timestamp;
     obj->id = id;
@@ -67,7 +85,7 @@ void ticker_insert_event(const ticker_data_t *const data, ticker_event_t *obj, t
     ticker_event_t *prev = NULL, *p = data->queue->head;
     while (p != NULL) {
         /* check if we come before p */
-        if ((int)(timestamp - p->timestamp) < 0) {
+        if(ticker_timeIsInPeriod(actCnt, timestamp, p->timestamp)) {
             break;
         }
         /* go to the next element */
@@ -117,4 +135,29 @@ void ticker_remove_event(const ticker_data_t *const data, ticker_event_t *obj) {
 timestamp_t ticker_read(const ticker_data_t *const data)
 {
     return data->interface->read();
+}
+
+//                                            actCnt        insertCnt          nextCnt
+inline bool ticker_timeIsInPeriod(timestamp_t start, timestamp_t time, timestamp_t end)
+{
+    // Taking care to handle wrapping:
+    //   Case (A.1)
+    //                       A    I   N
+    //      0 ---------------|----|---|-- 0xf
+    //
+    //   Case (A.2): this case also allows S==T==E
+    //         N                 A    I
+    //      0 -|-----------------|----|-- 0xf
+    //
+    //   Case (B)
+    //         I   N                 A
+    //      0 -|---|-----------------|--- 0xf
+    //
+    if((time >= start && ( time < end ||              // (A.1)
+                          start >= end)) ||           // (A.2)
+        (time < start && end < start && end > time))  // (B)
+    {
+        return true;
+    }
+    return false;
 }
